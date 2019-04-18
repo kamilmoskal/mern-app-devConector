7f3b56d53425713bf85eb6b20ee47e9a2547d21f
kamilmoskal <kamil_moskal@interia.pl>
2019-04-18 15:46:04 +0200

    Add Redux setup

---
+++ client/src/store/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
